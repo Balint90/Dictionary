41626fbedc57179e130777d3aa26b39dd6456479
balint.csertan <balint.csertan@pazirik.hu>
2021-11-27 22:35:05 +0100

    TryGet done

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary
 {
@@ -6,7 +7,83 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee[] employees =
+            {
+                new Employee("CEO", "Gwyn", 95, 200),
+                new Employee("Manager", "Joe", 35, 25),
+                new Employee("HR", "Lora", 32, 21),
+                new Employee("Secretary", "Petra", 28, 18),
+                new Employee("Lead Developer", "Artorias", 55, 35),
+                new Employee("Intern", "Ernest", 22, 8),
+            };
+
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            {
+                {1,"one" },
+                {2,"two"},
+                {3,"three" }
+            };
+
+            Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+
+            foreach (Employee employee in employees)
+            {
+                employeesDirectory.Add(employee.Role, employee);
+            }
+
+            for (int i = 0; i < employeesDirectory.Count; i++)
+            {
+                //KeyValuePair<string, Employee> keyValuePair = employeesDirectory;
+            }
+
+            string key = "CEO";
+            if (employeesDirectory.ContainsKey(key))
+            {
+                Employee emp = employeesDirectory[key];
+                Console.WriteLine("Employee name: {0}, role: {1}, salary: {2}", emp.Name, emp.Role, emp.Salary);
+            }
+            else
+            {
+                Console.WriteLine("There is no employee with key: {0}", key);
+            }
+
+            Employee result = null;
+
+            if (employeesDirectory.TryGetValue("Intern", out result))
+            {
+                Console.WriteLine("Value retrieved");
+
+                Console.WriteLine("Employee name: {0}", result.Name);
+                Console.WriteLine("Employee role: {0}", result.Role);
+                Console.WriteLine("Employee age: {0}", result.Age);
+                Console.WriteLine("Employee salary: {0}", result.Salary);
+            }
+            else
+            {
+                Console.WriteLine("The key does not exist");
+            }
+        }
+
+        class Employee
+        {
+            public string Role { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public float Rate { get; set; }
+
+            public float Salary { get
+                {
+                    return Rate * 8 * 5 * 4 * 12;
+                }
+            }
+
+            public Employee(string role, string name, int age, float rate)
+            {
+                this.Role = role;
+                this.Name = name;
+                this.Age = age;
+                this.Rate = rate;
+            }
         }
     }
 }
